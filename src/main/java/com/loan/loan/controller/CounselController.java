05c1a8e972481f5d6c6c3ee2be07605d1e76b3b0
MoonStar <moonsung0331@gmail.com>
2023-07-10 23:32:53 +0900

    feat: 대출 상담 요청을 받고 응답을 주기 위한 컨트롤러 구현

---
+++ src/main/java/com/loan/loan/controller/CounselController.java
@@ -0,0 +1,25 @@
+package com.loan.loan.controller;
+
+import com.loan.loan.dto.CounselDTO;
+import com.loan.loan.dto.CounselDTO.Request;
+import com.loan.loan.dto.CounselDTO.Response;
+import com.loan.loan.dto.ResponseDTO;
+import com.loan.loan.service.CounselService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/counsels")
+public class CounselController extends AbstractController {
+
+    private final CounselService counselService;
+
+    @PostMapping
+    public ResponseDTO<Response> create(@RequestBody Request request) {
+        return ok(counselService.create(request));
+    }
+}
