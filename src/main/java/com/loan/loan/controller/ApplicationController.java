df8f325f3b0d007efea76d298286dfa43db2647c
MoonStar <moonsung0331@gmail.com>
2023-07-14 15:30:28 +0900

    feat: 대출 신청 서류 등록 기능 구현
    대출 신청 서류 등록 시 파일 저장이 정상적으로 동작하는지 확인하기 위해서 아직은 validation 을 수행하지 않았다.

--- src/main/java/com/loan/loan/controller/ApplicationController.java
+++ src/main/java/com/loan/loan/controller/ApplicationController.java
@@ -1,13 +1,14 @@
 package com.loan.loan.controller;
 
-import com.loan.loan.dto.ApplicationDTO;
 import com.loan.loan.dto.ApplicationDTO.AcceptTerms;
 import com.loan.loan.dto.ApplicationDTO.Request;
 import com.loan.loan.dto.ApplicationDTO.Response;
 import com.loan.loan.dto.ResponseDTO;
 import com.loan.loan.service.ApplicationServiceImpl;
+import com.loan.loan.service.FileStorageServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class ApplicationController extends AbstractController {
 
     private final ApplicationServiceImpl applicationService;
+    private final FileStorageServiceImpl fileStorageService;
 
     @PostMapping
     public ResponseDTO<Response> create(@RequestBody Request request) {
@@ -40,5 +42,11 @@ public class ApplicationController extends AbstractController {
     @PostMapping("/{applicationId}/terms")
     public ResponseDTO<Boolean> acceptTerms(@PathVariable Long applicationId, @RequestBody AcceptTerms request) {
         return ok(applicationService.acceptTerms(applicationId, request));
+    }
+
+    @PostMapping("/files")
+    public ResponseDTO<Void> upload(MultipartFile file) {
+        fileStorageService.save(file);
+        return ok();
     }
 }
---
+++ src/main/java/com/loan/loan/service/FileStorageService.java
@@ -0,0 +1,8 @@
+package com.loan.loan.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStorageService {
+
+    void save(MultipartFile file);
+}
