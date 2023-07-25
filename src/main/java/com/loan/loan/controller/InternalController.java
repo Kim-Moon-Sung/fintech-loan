e3981ae4b3701c8d2a5512cd7e84e6c8ae77d163
MoonStar <moonsung0331@gmail.com>
2023-07-25 15:51:22 +0900

    feat: 대출 집행 등록 기능을 위한 내부 API 작성

---
+++ src/main/java/com/loan/loan/controller/InternalController.java
@@ -0,0 +1,21 @@
+package com.loan.loan.controller;
+
+import com.loan.loan.dto.EntryDTO.Request;
+import com.loan.loan.dto.EntryDTO.Response;
+import com.loan.loan.dto.ResponseDTO;
+import com.loan.loan.service.EntryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/internal/applications")
+public class InternalController extends AbstractController {
+
+    private final EntryService entryService;
+
+    @PostMapping("/{applicationId}/entries")
+    public ResponseDTO<Response> create(@PathVariable Long applicationId, @RequestBody Request request) {
+        return ok(entryService.create(applicationId, request));
+    }
+}
