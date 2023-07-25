efd71aec059e5d630bc62c1c7f841d45bee20024
MoonStar <moonsung0331@gmail.com>
2023-07-25 16:40:42 +0900

    feat: 대출 상환에 따른 잔고 처리를 위한 기능 구현

--- src/main/java/com/loan/loan/service/BalanceService.java
+++ src/main/java/com/loan/loan/service/BalanceService.java
@@ -1,5 +1,6 @@
 package com.loan.loan.service;
 
+import com.loan.loan.dto.BalanceDTO.RepaymentRequest;
 import com.loan.loan.dto.BalanceDTO.Request;
 import com.loan.loan.dto.BalanceDTO.Response;
 import com.loan.loan.dto.BalanceDTO.UpdateRequest;
@@ -9,4 +10,8 @@ public interface BalanceService {
     Response create(Long applicationId, Request request);
 
     Response update(Long applicationId, UpdateRequest request);
+
+    Response repaymentUpdate(Long applicationId, RepaymentRequest request);
+
+    void delete(Long applicationId);
 }
