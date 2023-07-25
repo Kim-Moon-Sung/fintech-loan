4cec7148aa206236ecf781ed9585f148df4c9969
MoonStar <moonsung0331@gmail.com>
2023-07-25 16:58:15 +0900

    feat: 대출 상환 전체 조회 기능 구현

--- src/main/java/com/loan/loan/service/RepaymentService.java
+++ src/main/java/com/loan/loan/service/RepaymentService.java
@@ -1,9 +1,14 @@
 package com.loan.loan.service;
 
+import com.loan.loan.dto.RepaymentDTO.ListResponse;
 import com.loan.loan.dto.RepaymentDTO.Request;
 import com.loan.loan.dto.RepaymentDTO.Response;
+
+import java.util.List;
 
 public interface RepaymentService {
 
     Response create(Long applicationId, Request request);
+
+    List<ListResponse> get(Long applicationId);
 }
